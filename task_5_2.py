35e33985fccbf508c535fad0ad0cb8209cde743a
romanvesel <roman.vesel@mail.ru>
2020-10-12 15:30:39 +0300

    homework_5

--- task_5_2.py
+++ task_5_2.py
@@ -1,7 +1,7 @@
 x = int(input('Enter number: '))
 total = 0
 new_list2 = []
-
+compos = 1
 if len(str(x)) < 2:
     print(x)
 else:
@@ -9,5 +9,8 @@ else:
     for el in new_list:
         new_list2.append(int(el))
         total = sum(new_list2)
+    for x in new_list2:
+        compos = x * compos
 
     print(total)
+    print(compos)
