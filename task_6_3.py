9fbc53b51feed958b7d8ca04e595af802ce6f9c9
romanvesel <roman.vesel@mail.ru>
2020-10-13 21:16:57 +0300

    homework_6

---
+++ task_6_3.py
@@ -0,0 +1,9 @@
+from random import randint
+
+a = int(input('Enter a: '))
+b = int(input('Enter b: '))
+n = int(input('Enter n: '))
+m = int(input('Enter m: '))
+matrix = [[randint(a, b) for i in range(n)] for j in range(m)]
+min_el = min(min(matrix))
+print(min_el)
