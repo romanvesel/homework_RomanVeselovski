4415e9c239a6ec637465f1002ec5c74400a9eb97
romanvesel <roman.vesel@mail.ru>
2020-10-13 21:48:26 +0300

    homework_6_task_6_4

--- task_6_1.py
+++ task_6_1.py
@@ -18,12 +18,23 @@ from random import randint
 # print(max_el)
 
 
+# a = int(input('Enter a: '))
+# b = int(input('Enter b: '))
+# n = int(input('Enter n: '))
+# m = int(input('Enter m: '))
+# matrix = [[randint(a, b) for i in range(n)] for j in range(m)]
+# min_el = min(min(matrix))
+# print(min_el)
+
+summa = 0
 a = int(input('Enter a: '))
 b = int(input('Enter b: '))
 n = int(input('Enter n: '))
 m = int(input('Enter m: '))
 matrix = [[randint(a, b) for i in range(n)] for j in range(m)]
-min_el = min(min(matrix))
-print(min_el)
+for x in matrix:
+    summa += sum(x)
+print(summa)
+print(matrix)
 
 
