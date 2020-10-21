31f6e1f50b9b90a6036132ee98f80224cf6a0990
romanvesel <roman.vesel@mail.ru>
2020-10-21 21:35:46 +0300

    homework_8

--- task_8_2.py
+++ task_8_2.py
@@ -1,10 +1,21 @@
-def check(some_word):
-    revers = some_word[::-1]
-    if some_word == revers:
-        print('Палиндром')
-    else:
-        print('Не палиндром')
+# def check(some_word):
+#     revers = some_word[::-1]
+#     if some_word == revers:
+#         print('Палиндром')
+#     else:
+#         print('Не палиндром')
 
 
-some_word = input('Enter word: ')
-check(some_word)
+# some_word = input('Enter word: ')
+
+
+def check_list():
+    for i, j in enumerate(some_list):
+        word = some_list[i]
+        if j[::-1] == word:
+            print(f'{word} - Палиндром')
+        else:
+            print(f'{word} - Не палиндром')
+
+some_list = ('mom', 'october', '121', '355')
+check_list()
