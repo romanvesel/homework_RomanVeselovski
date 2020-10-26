77aac6eadb9376b4f0f4b4f54d26464d4b7adccf
romanvesel <roman.vesel@mail.ru>
2020-10-26 13:20:23 +0300

    homework_9_task_4_fixed

--- task_9_4.py
+++ task_9_4.py
@@ -1,15 +1,16 @@
 some_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 def odd(func):
-    def wraps(*args):
+    def wraps(*args, **kwargs):
         new_list = []
         for i, j in enumerate(*args):
             new_list.append(j)
-        return func(*new_list[::-1])
+        return func(*new_list[::-1], f'Остальное - {kwargs}')
     return wraps
 
 @odd
 def some_func(*args):
     print(f'Отформатированный список - {args}')
 
-some_func(some_list)
+
+some_func(some_list, a=10, c=50)
