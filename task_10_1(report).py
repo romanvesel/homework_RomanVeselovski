5f38b19df9c1e2f9ae473c08657d42ca369ef977
romanvesel <roman.vesel@mail.ru>
2020-10-30 10:55:30 +0300

    homework_10_task_1

---
+++ task_10_1(report).py
@@ -0,0 +1,44 @@
+import csv
+filename = "task_10_1.csv"
+my_file = open(filename, mode='r')
+group_1 = []
+group_2 = []
+group_3 = []
+group_4 = []
+group_5 = []
+for rows in my_file.readlines()[1::]:
+    formated = rows.strip('\n')
+    formated = formated.split(',')
+    if int(formated[2]) <= 12:
+        group_1.append(rows.strip('\n'))
+    elif 18 >= int(formated[2]) > 12:
+        group_2.append(rows.strip('\n'))
+    elif 25 >= int(formated[2]) > 18:
+        group_3.append(rows.strip('\n'))
+    elif 40 >= int(formated[2]) > 25:
+        group_4.append(rows.strip('\n'))
+    elif int(formated[2]) > 40:
+        group_5.append(rows.strip('\n'))
+print(my_file.read())
+
+fields = ['1-12', '13-18', '19-25', '26-40', '40+']
+rows = [
+    group_1,
+    group_2,
+    group_3,
+    group_4,
+    group_5,
+]
+stat = [
+    f'Количество в возрастной группе: {len(group_1)}',
+    f'Количество в возрастной группе: {len(group_2)}',
+    f'Количество в возрастной группе: {len(group_3)}',
+    f'Количество в возрастной группе: {len(group_4)}',
+    f'Количество в возрастной группе: {len(group_5)}',
+        ]
+new_filename = "statistics.csv"
+with open(new_filename, 'w+') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(fields)
+    csvwriter.writerow(rows)
+    csvwriter.writerow(stat)
---
+++ task_10_1.py
@@ -0,0 +1,16 @@
+import csv
+fields = ['Имя', 'Фамилия', 'Возраст']
+rows = [
+    ['Roman', 'Veselovski', '6'],
+    ['Artem', 'Ivanov', '11'],
+    ['Max', 'Petrov', '21'],
+    ['Irina', 'Luberec', '17'],
+    ['Max', 'Ivanov', '66'],
+    ['Alex', 'Varkalov', '40'],
+    ['Dmitriy', 'Ivanov', '36'],
+]
+filename = "task_10_1.csv"
+with open(filename, 'w+') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(fields)
+    csvwriter.writerows(rows)
