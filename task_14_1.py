af68a002c3b75be86e5835be39d96b96e98dc3ba
romanvesel <roman.vesel@mail.ru>
2020-11-09 23:28:27 +0300

    homework_14_task_2(fixed)

--- task_14_1.py
+++ task_14_1.py
@@ -1,11 +1,16 @@
 import time
 import datetime
-alarm_name = input('Enter your name: ')
-alarm_time_h = int(input('Enter hours: '))
-alarm_time_m = int(input('Enter min: '))
-alarm_time_s = int(input('Enter sec: '))
+import argparse
 
-start_time = datetime.timedelta(hours=alarm_time_h, minutes=alarm_time_m, seconds=alarm_time_s)
+parser = argparse.ArgumentParser()
+parser.add_argument('alarm_name', type=str)
+parser.add_argument('alarm_time_h', type=int)
+parser.add_argument('alarm_time_m', type=int)
+parser.add_argument('alarm_time_s', type=int)
+args = parser.parse_args()
+
+
+start_time = datetime.timedelta(hours=args.alarm_time_h, minutes=args.alarm_time_m, seconds=args.alarm_time_s)
 run_time = datetime.timedelta(seconds=1)
 finish_time = datetime.timedelta(hours=0, minutes=0, seconds=0)
 
@@ -17,5 +22,5 @@ while start_time > finish_time:
         print('Alarm!')
 
 with open('log.txt', 'a+') as my_file:
-    my_file.writelines(f'{alarm_name} : {datetime.datetime.now()}\n')
+    my_file.writelines(f'{args.alarm_name} : {datetime.datetime.now()}\n')
     my_file.close()
